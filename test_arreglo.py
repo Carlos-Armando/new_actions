816e61564566641ff2fa3466491a8ed96b3b6e0e
Carlos-Armando <50434581+Carlos-Armando@users.noreply.github.com>
2021-08-08 21:42:11 -0500

    Update test_arreglo.py

--- test_arreglo.py
+++ test_arreglo.py
@@ -1,1 +1,11 @@
+import pytest
+import arreglo as ar
 
+array = [5,3,4,2,1]
+diccionario = [{"nombre":"Fernando","edad":23},{"nombre":"Ruben","edad":15},{"nombre":"Fernando","edad":43}]
+
+def test_arreglo():
+    assert ar.acomodo(array) == [1,2,3,4,5]
+
+def test_contar_mayores():
+    assert ar.contar_mayores(diccionario) == 2
